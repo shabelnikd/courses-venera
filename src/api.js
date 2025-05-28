cf6d6c19f635544646704ecfcbae41edfd29d9a9
shabelnikd <shabelnikdanil.main@gmail.com>
2025-05-29 00:17:18 +0600

    api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'https://venera.diplomcomtehno.online/',
 });
 
 api.interceptors.request.use((config) => {
